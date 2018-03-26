2743ac7b74e20b878ccb7ef463f6120f5925ab74
Windows <gersoncalienes@gmail.com>
2018-03-26 17:35:38 -0400

    Added folders necessary for possible database scheme

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL =
-	process.env.DATABASE_URL ||
-	global.DATABASE_URL ||
-	'mongodb://synapse:12345678@ds223609.mlab.com:23609/synapse-hackathon';
+  process.env.MONGODB_URI ||
+  global.MONGO_URI ||
+  'mongodb://synapse:12345678@ds223609.mlab.com:23609/synapse-hackathon';
 
 exports.PORT = process.env.PORT || 8080;
