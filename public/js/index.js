d4fca77dfd9c6f00248ac5f72cff4258ec6d8752
Windows <gersoncalienes@gmail.com>
2018-03-28 19:56:48 -0400

    set constants in index.js for html and linked jquery to html

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  const $name = $('#js-name');
+  const $address = $('#js-address');
+  const $location = $('#js-location');
+  const $times = $('#js-times');
+  const $days = $('#js-days');
+  const $other = $('#js-other');
+};
