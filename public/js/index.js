fb2c929b69fdeada11e7ea5a5b284aedfd67a1a8
Windows <gersoncalienes@gmail.com>
2018-03-28 20:03:16 -0400

    Added the frontend call to post

--- public/js/index.js
+++ public/js/index.js
@@ -5,4 +5,27 @@ window.onload = function() {
   const $times = $('#js-times');
   const $days = $('#js-days');
   const $other = $('#js-other');
+
+  $('#submit').on('click', function(event) {
+    event.preventDefault();
+    const data = {
+      name: $name.val(),
+      address: $address.val(),
+      location: $location.val(),
+      times: $times.val(),
+      days: $days.val(),
+      other: $other.val()
+    };
+    $.ajax({
+      type: 'POST',
+      url: '/api',
+      data: data,
+      success: function(newData) {
+        console.log(newData);
+      },
+      error: function() {
+        alert('Something went wrong please try again later.');
+      }
+    });
+  });
 };
